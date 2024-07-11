2ec18910fb4abdc6ccbde66a2bb13cddd5772d4a
Fabian Gonzalez <fabiangonzalezcode@gmail.com>
2024-07-11 01:45:09 -0400

    Avances iniciales

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    console.log('El DOM se ha cargado completamente');
+
+    //Mensaje de bienvenida
+    const mensajeBienvenida = 'Bienvenido a mi portafolio';
+    alert(mensajeBienvenida);
+});
